bafb622ad6a019c97f8695c199c4a6bedcb12b69
Isaac Medina <isaac.meedinaa@gmail.com>
2020-08-06 19:32:05 -0500

    added metatags

--- src/components/ContactMe.js
+++ src/components/ContactMe.js
@@ -11,12 +11,39 @@ class ContactMe extends Component {
       >
         <Row>
           <Col className="contact-me-header-container">
-            <h1 className="contact-me-title secondary">ContactMe</h1>
+            <h1 className="contact-me-title secondary">Contact Me</h1>
             <div className="section-block background-primary"></div>
           </Col>
         </Row>
         <Row>
-          <Col md={true} className="contact-me-content-one"></Col>
+          <Col md={true} className="contact-me-content-one">
+            <form
+              className="contact-me-form"
+              action="https://formspree.io/xbjzwvrr"
+              method="POST"
+            >
+              <input
+                className="contact-me-input"
+                placeholder="Full Name"
+                name="fullname"
+              />
+              <input
+                className="contact-me-input"
+                placeholder="Subject"
+                name="subject"
+              />
+              <textarea
+                className="contact-me-textarea"
+                placeholder="Message"
+                name="message"
+              />
+              <input
+                className="contact-me-btn background-primary"
+                value="Send"
+                type="submit"
+              />
+            </form>
+          </Col>
         </Row>
       </Container>
     );
